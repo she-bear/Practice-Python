50ea0a029d8cd6a4814ff1af4f0cb7fd962f81a2
Kutueva Olga <kutueva.olga@gmail.com>
2023-01-09 17:40:21 +0300

    Задача 1 переделана на единый дескриптор

--- Task_10/task_10_1.py
+++ Task_10/task_10_1.py
@@ -1,30 +1,12 @@
-class CheckRoadWidth:
-    def __get__(self, instance, owner):
-        return instance.__dict__[self.my_attr]
+class CheckParameter:
+    def __init__(self, min_v=None, max_v=None):
+        self.min_v = min_v
+        self.max_v = max_v
 
     def __set__(self, instance, value):
-        if value < 6 or value > 30:
-            raise ValueError("Неверная ширина дороги!")
+        if value < self.min_v or value > self.max_v:
+            raise ValueError("Неверный параметр!")
         instance.__dict__[self.my_attr] = value
-
-    def __delete__(self, instance):
-        del instance.__dict__[self.my_attr]
-
-    def __set_name__(self, owner, my_attr):
-        self.my_attr = my_attr
-
-
-class CheckRoadLength:
-    def __get__(self, instance, owner):
-        return instance.__dict__[self.my_attr]
-
-    def __set__(self, instance, value):
-        if value < 0 or value > 100000:
-            raise ValueError("Неверная длина дороги!")
-        instance.__dict__[self.my_attr] = value
-
-    def __delete__(self, instance):
-        del instance.__dict__[self.my_attr]
 
     def __set_name__(self, owner, my_attr):
         self.my_attr = my_attr
@@ -32,9 +14,9 @@ class CheckRoadLength:
 
 class Road:
     # считаем, что мы делаем расчет только для дорог шириной от 6 до 30 м
-    width = CheckRoadWidth()
+    width = CheckParameter(6, 30)
     # считаем, что длина дороги может быть не более 100 км (100 000 м)
-    length = CheckRoadLength()
+    length = CheckParameter(0, 100000)
 
     def __init__(self, width, length):
         self.width = width
@@ -45,7 +27,7 @@ class Road:
 
 
 # отработает верно
-road_obj = Road(10, 20000)
+road_obj = Road(6, 20000)
 asphalt_mass = road_obj.get_asphalt_mass_calc(25, 5)
 print(f"Общая масса асфальта: {asphalt_mass} т")
 
